3b6a6d26210fbc6d6558814240818112eb2b9d5e
AdamFedoruk650 <56296648+AdamFedoruk650@users.noreply.github.com>
2023-10-13 14:45:59 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,102 @@
+import tkinter as tk
+from tkinter import filedialog
+from PIL import Image, ImageDraw, ImageFont, ImageTk
+
+def add_watermark():
+    # Wczytaj obraz źródłowy
+    file_path = filedialog.askopenfilename()
+    source_image = Image.open(file_path)
+
+    # Wprowadź tekst znaku wodnego
+    watermark_text = watermark_entry.get()
+
+    # Ustaw parametry znaku wodnego
+    watermark_color = watermark_color_var.get()
+    watermark_position = watermark_position_var.get()
+    watermark_size = int(watermark_size_entry.get())
+
+    # Skopiuj obraz źródłowy, aby nie modyfikować oryginału
+    marked_image = source_image.copy()
+
+    # Przygotuj obraz do rysowania na nim znaku wodnego
+    draw = ImageDraw.Draw(marked_image)
+    font = ImageFont.load_default()  # Domyślna czcionka
+
+    # Określ pozycję znaku wodnego
+    if watermark_position == "Góra lewo":
+        position = (10, 10)
+    elif watermark_position == "Góra prawo":
+        position = (source_image.width - watermark_size - 10, 10)
+    elif watermark_position == "Dół lewo":
+        position = (10, source_image.height - watermark_size - 10)
+    else:
+        position = (source_image.width - watermark_size - 10, source_image.height - watermark_size - 10)
+
+    # Rysuj znak wodny
+    draw.text(position, watermark_text, fill=watermark_color, font=font)
+
+    # Wyświetl obraz w interfejsie
+    marked_photo = ImageTk.PhotoImage(marked_image)
+    marked_image_label.config(image=marked_photo)
+    marked_image_label.photo = marked_photo
+
+# Tworzenie głównego okna
+root = tk.Tk()
+root.title("Aplikacja do dodawania znaku wodnego")
+
+# Przycisk wyboru obrazu źródłowego
+select_image_button = tk.Button(root, text="Wybierz obraz źródłowy", command=add_watermark)
+select_image_button.pack()
+
+# Pole tekstowe na znak wodny
+watermark_entry = tk.Entry(root, width=30)
+watermark_entry.pack()
+watermark_entry.insert(0, "Znak wodny")
+
+# Przyciski wyboru koloru znaku wodnego
+watermark_color_label = tk.Label(root, text="Kolor znaku wodnego:")
+watermark_color_label.pack()
+
+watermark_color_var = tk.StringVar()
+watermark_color_var.set("black")
+
+black_color_radio = tk.Radiobutton(root, text="Czarny", variable=watermark_color_var, value="black")
+black_color_radio.pack()
+red_color_radio = tk.Radiobutton(root, text="Czerwony", variable=watermark_color_var, value="red")
+red_color_radio.pack()
+blue_color_radio = tk.Radiobutton(root, text="Niebieski", variable=watermark_color_var, value="blue")
+blue_color_radio.pack()
+
+# Przyciski wyboru położenia znaku wodnego
+watermark_position_label = tk.Label(root, text="Położenie znaku wodnego:")
+watermark_position_label.pack()
+
+watermark_position_var = tk.StringVar()
+watermark_position_var.set("Góra lewo")
+
+top_left_radio = tk.Radiobutton(root, text="Góra lewo", variable=watermark_position_var, value="Góra lewo")
+top_left_radio.pack()
+top_right_radio = tk.Radiobutton(root, text="Góra prawo", variable=watermark_position_var, value="Góra prawo")
+top_right_radio.pack()
+bottom_left_radio = tk.Radiobutton(root, text="Dół lewo", variable=watermark_position_var, value="Dół lewo")
+bottom_left_radio.pack()
+bottom_right_radio = tk.Radiobutton(root, text="Dół prawo", variable=watermark_position_var, value="Dół prawo")
+bottom_right_radio.pack()
+
+# Pole wprowadzenia rozmiaru znaku wodnego
+watermark_size_label = tk.Label(root, text="Rozmiar znaku wodnego:")
+watermark_size_label.pack()
+
+watermark_size_entry = tk.Entry(root, width=5)
+watermark_size_entry.pack()
+watermark_size_entry.insert(0, "24")
+
+# Przycisk do generowania obrazu z znakiem wodnym
+generate_button = tk.Button(root, text="Generuj obraz z znakiem wodnym", command=add_watermark)
+generate_button.pack()
+
+# Wyświetlanie obrazu
+marked_image_label = tk.Label(root)
+marked_image_label.pack()
+
+root.mainloop()
